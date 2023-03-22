bf1989b8a86b2c9fc053010fd56b6c9be6f2f056
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-22 16:42:34 +0000

    dropped in template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file generates user input
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("streetName").value
+  const streetNumber = parseInt(document.getElementById("streetNumber").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your address is: " + streetNumber + " " + streetName + "."
+}
